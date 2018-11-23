436615fbd3b4ad25d709553ecf656adbfdf2384a
Piotr Nastaj <piotr@nastaj.net>
2018-11-23 13:37:31 +0100

    Merge branch 'master' into WDP181002-29 (footer section is broken) # Conflicts: #	src/index.html #	src/sass/style.scss

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -6,3 +6,59 @@ $('.star-rank').click(function () {
 	$(this).nextAll().addClass('star-full');
 	$(this).prevAll().addClass('star-outline');
 })
+
+
+const DOM = {};
+
+DOM.furnitureSlider = document.getElementById("furniture-slider");
+DOM.furnitureSliderDots = document.getElementById("furniture-dots");
+
+DOM.blogSlider = document.getElementById("blog-slider");
+DOM.blogSliderDots = document.getElementById("blog-dots");
+
+const furnitureSectionSlider = tns({
+	container: DOM.furnitureSlider,
+	items: 1,
+	slideBy: 'page',
+	controls: false,
+	autoplay: true,
+	autoplayButtonOutput: false,
+	navContainer: DOM.furnitureSliderDots,
+	mouseDrag: true,
+	responsive: {
+		576: {
+			items: 2
+		},
+		992: {
+			items: 3
+		},
+		1200: {
+			items: 4
+		}
+	}
+
+});
+
+
+const blogSectionSlider = tns({
+	container: DOM.blogSlider,
+	items: 1,
+	slideBy: 'page',
+	controls: false,
+	autoplay: true,
+	autoplayButtonOutput: false,
+	navContainer: DOM.blogSliderDots,
+	mouseDrag: true,
+	responsive: {
+		576: {
+			items: 2
+		},
+		992: {
+			items: 3
+		}
+	}
+
+});
+
+furnitureSectionSlider();
+blogSectionSlider();
